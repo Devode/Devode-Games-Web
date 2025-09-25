70a4cd67ed6e649cf29775a761f7f7bd87d108f7
Devode <3975493600@qq.com>
2025-09-25 23:30:31 +0800

    修改方块跑酷游戏截图为1.0.4-beta-2版本游戏截图，修改英雄区域段落文本将glass-header改为半透明黑色背景，将游戏下载json改为从其它文件夹导入

--- js/downloads_script.js
+++ js/downloads_script.js
@@ -1,4 +1,4 @@
-const data = {
+/* const data = {
   "BlockyParkour": {
     "name": "方块跑酷",
     "platforms": {
@@ -13,9 +13,23 @@ const data = {
       }
     }
   }
-};
+}; */
 
 const hashkey = window.location.hash.substring(1);
+
+async function loadJSON() {
+  try {
+    const response = await fetch('../json/downloads.json');
+    if (!response.ok) {
+      throw new Error('网络响应不正常');
+    }
+    data = await response.json();
+    console.log(data); // 处理获取到的数据
+  } catch (error) {
+    console.error('获取数据失败:', error);
+  }
+}
+
 
 function setCard(platform) {
   const newCard = document.createElement('div');
@@ -50,12 +64,25 @@ function setCard(platform) {
   return newCard;
 };
 
-addEventListener('DOMContentLoaded', (event) => {
+function updatePage() {
+  const platforms = Object.keys(data[hashkey]["platforms"]);
   document.title = "Devode Games - 下载" + data[hashkey]["name"];
   document.getElementById("downloads-section-title").innerText = "下载" + data[hashkey]["name"];
   const cardGrid = document.getElementById("downloads-grid");
   
-  cardGrid.appendChild(setCard("Android"));
-  cardGrid.appendChild(setCard("Windows"))
+  for (const platform in platforms) {
+    if (platforms.hasOwnProperty(platform)) {
+      cardGrid.appendChild(setCard(platforms[platform]));
+    }
+  }
+  /* cardGrid.appendChild(setCard("Android"));
+  cardGrid.appendChild(setCard("Windows")) */
+}
 
-});
+(async function initializeApp() {
+    // 等待JSON文件加载完成
+    await loadJSON();
+
+    // 只有在JSON加载完成后，才执行后续函数
+    updatePage();
+})();
